5391a811afd23b3809f00a1d94a949d7c4d63187
Exusiai101 <100024009+Exusiai101@users.noreply.github.com>
2026-05-17 18:31:45 +0800

    refactor(models): drop unused foreign import

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime, UTC
 from enum import Enum
 from sqlalchemy import CheckConstraint
 from flask_login import UserMixin
-from sqlalchemy.orm import foreign, relationship
+from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
